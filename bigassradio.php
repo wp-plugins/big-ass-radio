95261fe05f6bb6fb1487511a9246f6fa76e60bd0
146st <146st@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 02:44:26 +0000

    First upload Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/big-ass-radio/trunk@1120986 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bigassradio.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: Big Ass Radio
+Description: Add a Big Ass Radio to your wordpress website. More than 15,000 Big Ass Stations in 30+ genres. 
+Version: 1.0
+*/
+
+// Create class
+class Radio_widget extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+		'wpb_widget', 
+		__('Big Ass Radio', 'wpb_widget_domain'), // Widget name will appear in UI
+		array( 'description' => __( 'Add the Big Ass Radio widget to your website', 'wpb_widget_domain' ), )
+		);// Widget description
+	}//End function
+
+	// Widget front-end
+	public function widget( $args, $instance ) {
+	
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		// Before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+			
+			// Display the output
+			echo "<center><iframe src='http://bigassradio.com/embed/player.php' style='height: 350px; width: 210px; border: 0px;' scrolling='no'></iframe></center>";
+			echo $args['after_widget'];
+		}//End if
+		
+		// Widget Backend 
+		public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}else{
+			$title = __( 'Big Ass Radio', 'wpb_widget_domain' );
+		}//End if
+
+	}//End function
+	
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}//End function
+	
+} // End class
+
+// Register and load the widget
+function wpb_load_widget() {
+	register_widget( 'Radio_widget' );
+}//End function
+add_action( 'widgets_init', 'wpb_load_widget' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Big Ass Radio Widget ===
+Contributors: Fourteen Six Street
+Tags: streaming radio, streaming, radio, music, entertainment, mp3 player, mp3 stream, mp3, big, ass, radio, big ass radio
+Requires at least: 3.0
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later.
+License URI: http://www.gnu.org/licenses/license-list.html#GPLCompatibleLicenses
+
+Big Ass Radio widget. 15,000 stations in more than 30 genres.
+
+== Description ==
+Big Ass Radio gives your visitors access to 15,000 radio stations, podcasts and more. Works on tablets and smartphones. Super easy to use... Select a Genre, select a station, dance in the street.
+= What's new in verson 1.0 of this plugin? =
+1. Everything! New features coming soon...
+
+== Installation ==
+1. Download and unzip bigassradio.zip 
+1. Upload the FOLDER containing `bigassradio.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. To add the radio widget on a sidebar, browse to `Appearance > Widgets` and add 'Big Ass Radio' to desired sidebar.
+1. To add the radio widget to any post or page you will probably need another plugin such as the 'Widgets in pages' plugin. Direct support for this will be coming in a later release.
+
+== Frequently Asked Questions ==
+
+= Can I customize / skin the player? =
+
+No. 
+
+= Can I add my own content? =
+
+No, but maybe. Contact us if for options.
+
+Custom mods are handled by Fourteen Six Street, LLC. Contact by emailing info@146st.com.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial public release.
+
+== Additional info ==
+
+Our website http://bigassradio.com. 
+May the force be with you!
+ 
+
